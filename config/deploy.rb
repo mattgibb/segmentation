3ebb4db7356123385e7fdffa01197dba27783f42
Matt Gibb <powermoveguru@gmail.com>
2010-09-16 13:14:05 +0100

    Code deploys to heart via Capistrano.

---
+++ config/deploy.rb
@@ -0,0 +1,17 @@
+set :application, "segmentation"
+set :repository, "git://github.com/mattgibb/segmentation.git"
+set :scm, :git
+set :deploy_via, :copy
+set :copy_cache, true
+set :deploy_to, "~/imaging/#{application}"
+set :use_sudo, false
+
+role :servers, "heart"
+# role :clpcs, "work"
+
+# set :local_dir, "/Users/matthewgibb/Code/imaging/registration"
+
+desc "Compile all the itk programs on the servers"
+task :compile do
+  run "cd #{deploy_to}/itk; make"
+end
