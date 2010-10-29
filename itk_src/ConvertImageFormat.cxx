b3c44d439ec6c50497461712d9891ca0a324e466
Matt Gibb <powermoveguru@gmail.com>
2010-10-29 12:20:19 +0100

    ConvertImageFormat works.

--- itk_src/ConvertImageFormat.cxx
+++ itk_src/ConvertImageFormat.cxx
@@ -2,7 +2,7 @@
 
 #include "itkImage.h"
 #include "itkImageFileReader.h"
-#include "itkNearestNeighborInterpolateImageFunction.h"
+#include "itkImageFileWriter.h"
 
 using namespace std;
 
@@ -10,6 +10,7 @@ namespace po = boost::program_options;
 
 int main(int argc, char * argv[])
 {
+  // PARSE COMMAND LINE OPTIONS
   po::positional_options_description pd;
   pd.add("input", 1).add("output", 1);
   
@@ -29,7 +30,41 @@ int main(int argc, char * argv[])
   if ( vm.count("help") || !vm.count("input") || !vm.count("output") ) {
     cout << "\nUsage: " << argv[0] << " input output\n\n";
     cout << desc << "\n";
-    return 0;
+    return EXIT_FAILURE;
   }
+  
+  
+  // ITK STUFF
+  typedef short      PixelType;
+  const   unsigned int        Dimension = 3;
+  typedef itk::Image< PixelType, Dimension >    ImageType;
+
+  typedef itk::ImageFileReader< ImageType >  ReaderType;
+  typedef itk::ImageFileWriter< ImageType >  WriterType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  WriterType::Pointer writer = WriterType::New();
+
+  const char * inputFilename  = argv[1];
+  const char * outputFilename = argv[2];
+
+  reader->SetFileName( inputFilename  );
+  writer->SetFileName( outputFilename );
+
+  writer->SetInput( reader->GetOutput() );
+
+  try 
+    { 
+    writer->Update(); 
+    } 
+  catch( itk::ExceptionObject & err ) 
+    { 
+    std::cerr << "ExceptionObject caught !" << std::endl; 
+    std::cerr << err << std::endl; 
+    return EXIT_FAILURE;
+    } 
+    
+  return EXIT_SUCCESS;
+  
   
 }
